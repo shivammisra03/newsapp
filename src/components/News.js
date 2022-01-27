9d1b5376e23ca1dcec31ad97f60abdb72caceb5c
Shivam Misra <shivam.misra03@gmail.com>
2022-01-27 12:22:47 +0530

    react : tutorial 23

---
+++ src/components/News.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import NewsItems from './NewsItems'
+
+export class News extends Component {
+    render() {
+        return (
+            <div>
+                This is a news component
+                <NewsItems />
+                <NewsItems />
+                <NewsItems />
+                <NewsItems />
+                <NewsItems />
+                <NewsItems />
+            </div>
+        )
+    }
+}
+
+export default News
