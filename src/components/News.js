a2774fe70c15838717b06b2e37204854a3951a3f
Shivam Misra <shivam.misra03@gmail.com>
2022-01-29 18:57:56 +0530

    react tutorial 29

--- src/components/News.js
+++ src/components/News.js
@@ -1,63 +1,71 @@
 import React, { Component } from 'react'
 import NewsItems from './NewsItems'
+import Spinner from './Spinner'
 
 export class News extends Component {
-    
+
     constructor() {
         super()
         console.log("Hello im a constructor")
         this.state = {
             articles: [],
-            page : 1,
-            totalResults : 0
+            page: 1,
+            totalResults: 0,
+            loading: false
         }
+
     }
 
     async componentDidMount() {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=97b727ed6f9f4c4c96f60ae2a43669f7&page=1&pageSize=20`;
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=97b727ed6f9f4c4c96f60ae2a43669f7&page=1&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true })
         let data = await fetch(url);
         let parsedData = await data.json()
         console.log("parsed data :", parsedData)
-        this.setState({ articles: parsedData.articles, totalResults : parsedData.totalResults})
+        this.setState({ 
+            articles: parsedData.articles, 
+            totalResults: parsedData.totalResults, 
+            loading: false })
 
     }
 
-    handlePrevChange = async() => {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=97b727ed6f9f4c4c96f60ae2a43669f7&page=${this.state.page - 1}&pageSize=20`;
+    handlePrevChange = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=97b727ed6f9f4c4c96f60ae2a43669f7&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true })
         let data = await fetch(url);
         let parsedData = await data.json()
         this.setState({
+            loading: false,
             page: this.state.page - 1,
             articles: parsedData.articles
         })
     }
 
     allResultsShown = () => {
-        return this.state.page + 1 > Math.ceil(this.state.totalResults / 20)
+        return this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)
     }
 
     handleNextChange = async () => {
-
-        if(this.allResultsShown()){
-            console.log("All results shown")
-        } else {
-            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=97b727ed6f9f4c4c96f60ae2a43669f7&page=${this.state.page + 1}&pageSize=20`;
-            let data = await fetch(url);
-            let parsedData = await data.json()
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=97b727ed6f9f4c4c96f60ae2a43669f7&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+        this.setState({loading : true})
+        let data = await fetch(url);
+        let parsedData = await data.json()
         this.setState({
-                page: this.state.page + 1,
-                articles: parsedData.articles
-            })
-        }
-        
+            loading: false,
+            page: this.state.page + 1,
+            articles: parsedData.articles
+        })
+
+
     }
 
     render() {
         return (
             <div className='container my-3'>
-                <h1>News Monkey - Top Headline</h1>
+                <h1 className="text-center">News Monkey - Top Headline</h1>
+                {this.state.loading && <Spinner />}
                 <div className="row">
-                    {this.state.articles.map((element) => {
+                    {!this.state.loading && this.state.articles.map((element) => {
                         return <div className="col-md-4" key={element.url}>
                             <NewsItems title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage ? element.urlToImage : "https://images.indianexpress.com/2022/01/mars-1.jpg"}
                                 newsUrl={element.url} />
