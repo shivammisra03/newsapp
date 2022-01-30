7d4db5167ae0238bdb0aa9e690f552d88e535ef6
Shivam Misra <shivam.misra03@gmail.com>
2022-01-30 21:38:35 +0530

    react : tutorial 33

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -8,13 +8,13 @@ export class NewsItems extends Component {
     return (
       <div className='my-3'>
             <div className="card">
-          <span class="position-absolute top-0 translate-middle badge rounded-pill bg-success" style={{ left: '90%', zIndex : '1'}}>
+          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-success" style={{ left: '90%', zIndex : '1'}}>
             {source}</span>
                 <img src={imageUrl} className="card-img-top" alt="..."/>
                     <div className="card-body">
             <h5 className="card-title">{title}{title.length < 45 ? "" : "..."}</h5>
                         <p className="card-text">{description}{description.length < 88 ? "" : "..."}</p>
-                        <p class="card-text"><small class="text-muted">By {author ? author : "Unknown"} on {new Date(date).toGMTString()}</small></p>
+                        <p className="card-text"><small className="text-muted">By {author ? author : "Unknown"} on {new Date(date).toGMTString()}</small></p>
             <a href={newsUrl} target="_blank" className="btn btn-sm btn-success">Read More</a>
                     </div>
             </div>
