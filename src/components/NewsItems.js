b0b9525b9a568a0f9eda856a62e70d50e099b258
Shivam Misra <shivam.misra03@gmail.com>
2022-01-31 21:45:30 +0530

    react : tutorial 36

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react'
 
 export class NewsItems extends Component {
-    
+
   render() {
 
-    let {title, description, imageUrl, newsUrl, author, date, source} = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
     return (
       <div className='my-3'>
-            <div className="card">
-          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-success" style={{ left: '90%', zIndex : '1'}}>
-            {source}</span>
-                <img src={imageUrl} className="card-img-top" alt="..."/>
-                    <div className="card-body">
+        <div className="card">
+          <div style={{ display: 'flex', left: '0', position: 'absolute' }}>
+            <span className="badge rounded-pill bg-success">
+              {source}</span>
+          </div>
+
+          <img src={imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
             <h5 className="card-title">{title}{title.length < 45 ? "" : "..."}</h5>
-                        <p className="card-text">{description}{description.length < 88 ? "" : "..."}</p>
-                        <p className="card-text"><small className="text-muted">By {author ? author : "Unknown"} on {new Date(date).toGMTString()}</small></p>
+            <p className="card-text">{description}{description.length < 88 ? "" : "..."}</p>
+            <p className="card-text"><small className="text-muted">By {author ? author : "Unknown"} on {new Date(date).toGMTString()}</small></p>
             <a href={newsUrl} target="_blank" className="btn btn-sm btn-success">Read More</a>
-                    </div>
-            </div>
+          </div>
+        </div>
       </div>
     )
   }
